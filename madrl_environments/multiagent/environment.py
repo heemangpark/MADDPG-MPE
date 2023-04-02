ed2df6bb240308939782e32cf64d4e86d7235a7e
heemangpark <heemang0826@kaist.ac.kr>
2023-04-02 23:08:49 +0900

    setVisOption

--- madrl_environments/multiagent/environment.py
+++ madrl_environments/multiagent/environment.py
@@ -199,69 +199,58 @@ class MultiAgentEnv(gym.Env):
         self.render_geoms_xform = None
 
     # render environment
-    def render(self, mode='human', close=False):
-        # if mode == 'human':
-        #     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        #     message = ''
-        #     for agent in self.world.agents:
-        #         comm = []
-        #         for other in self.world.agents:
-        #             if other is agent: continue
-        #             if np.all(other.state.c == 0):
-        #                 word = '_'
-        #             else:
-        #                 word = alphabet[np.argmax(other.state.c)]
-        #             message += (other.name + ' to ' + agent.name + ': ' + word + '   ')
-        #     print(message)
+    def render(self, mode=None, close=True, vis=False):
+        if vis:
+            for i in range(len(self.viewers)):
+                # create viewers (if necessary)
+                if self.viewers[i] is None:
+                    # import rendering only if we need it (and don't import for headless machines)
+                    from madrl_environments.multiagent import rendering
+                    self.viewers[i] = rendering.Viewer(700, 700)
 
-        for i in range(len(self.viewers)):
-            # create viewers (if necessary)
-            if self.viewers[i] is None:
+            # create rendering geometry
+            if self.render_geoms is None:
                 # import rendering only if we need it (and don't import for headless machines)
+                # from gym.envs.classic_control import rendering
                 from madrl_environments.multiagent import rendering
-                self.viewers[i] = rendering.Viewer(700, 700)
+                self.render_geoms = []
+                self.render_geoms_xform = []
+                for entity in self.world.entities:
+                    geom = rendering.make_circle(entity.size)
+                    xform = rendering.Transform()
+                    if 'agent' in entity.name:
+                        geom.set_color(*entity.color, alpha=0.5)
+                    else:
+                        geom.set_color(*entity.color)
+                    geom.add_attr(xform)
+                    self.render_geoms.append(geom)
+                    self.render_geoms_xform.append(xform)
 
-        # create rendering geometry
-        if self.render_geoms is None:
-            # import rendering only if we need it (and don't import for headless machines)
-            # from gym.envs.classic_control import rendering
-            from madrl_environments.multiagent import rendering
-            self.render_geoms = []
-            self.render_geoms_xform = []
-            for entity in self.world.entities:
-                geom = rendering.make_circle(entity.size)
-                xform = rendering.Transform()
-                if 'agent' in entity.name:
-                    geom.set_color(*entity.color, alpha=0.5)
+                # add geoms to viewer
+                for viewer in self.viewers:
+                    viewer.geoms = []
+                    for geom in self.render_geoms:
+                        viewer.add_geom(geom)
+
+            results = []
+            for i in range(len(self.viewers)):
+                # update bounds to center around agent
+                cam_range = 1
+                if self.shared_viewer:
+                    pos = np.zeros(self.world.dim_p)
                 else:
-                    geom.set_color(*entity.color)
-                geom.add_attr(xform)
-                self.render_geoms.append(geom)
-                self.render_geoms_xform.append(xform)
+                    pos = self.agents[i].state.p_pos
+                self.viewers[i].set_bounds(pos[0] - cam_range, pos[0] + cam_range, pos[1] - cam_range,
+                                           pos[1] + cam_range)
+                # update geometry positions
+                for e, entity in enumerate(self.world.entities):
+                    self.render_geoms_xform[e].set_translation(*entity.state.p_pos)
+                # render to display or array
+                results.append(self.viewers[i].render(return_rgb_array=mode == 'rgb_array'))
 
-            # add geoms to viewer
-            for viewer in self.viewers:
-                viewer.geoms = []
-                for geom in self.render_geoms:
-                    viewer.add_geom(geom)
-
-        results = []
-        for i in range(len(self.viewers)):
-            # update bounds to center around agent
-            cam_range = 1
-            if self.shared_viewer:
-                pos = np.zeros(self.world.dim_p)
-            else:
-                pos = self.agents[i].state.p_pos
-            self.viewers[i].set_bounds(pos[0] - cam_range, pos[0] + cam_range, pos[1] - cam_range, pos[1] + cam_range)
-            # update geometry positions
-            for e, entity in enumerate(self.world.entities):
-                self.render_geoms_xform[e].set_translation(*entity.state.p_pos)
-            # render to display or array
-            results.append(self.viewers[i].render(return_rgb_array=mode == 'rgb_array'))
-
-        return results
-        # return None
+            return results
+        else:
+            return None
 
     # create receptor field locations in local coordinate frame
     def _make_receptor_locations(self, agent):
